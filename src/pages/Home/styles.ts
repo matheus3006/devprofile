56e924b4b329bd6669f66fd188e8340ae938827d
matheus3006 <matheussouza18@live.com>
2022-07-10 11:32:55 -0300

    feat: add style to Header component

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -17,30 +17,35 @@ export const Header = styled.View`
 `;
 
 export const UserWrapper = styled.View`
-width: 100%;
-padding: 0 ${RFValue(24)}px;
+  width: 100%;
+  padding: 0 ${RFValue(24)}px;
 `;
 
 export const UserInfo = styled.View`
-
+  flex-direction: row;
+  align-items: center;
 `;
 
-export const UserAvatarButton = styled.TouchableOpacity`
-
-`;
+export const UserAvatarButton = styled.TouchableOpacity``;
 
 export const UserAvatar = styled.Image`
-
+  width: ${RFValue(52)}px;
+  height: ${RFValue(52)}px;
+  border-radius: ${RFValue(10)}px;
 `;
 
 export const UserInfoDetail = styled.View`
-
+  margin-left: ${RFValue(17)}px;
 `;
 export const UserGreating = styled.Text`
-
+  color: ${({ theme }) => theme.colors.gray800};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
 `;
 
 export const UserName = styled.Text`
-
+  color: ${({ theme }) => theme.colors.gray800};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-weight: bold;
 `;
-
