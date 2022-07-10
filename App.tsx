5e3e913ba68e1b37cb105cbed826371f9c1c3452
matheus3006 <matheussouza18@live.com>
2022-07-09 21:50:54 -0300

    feat: add google fonts to project

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
 import { Home } from './src/pages/Home';
 
 const App: React.FunctionComponent = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <ThemeProvider theme={theme}>
       <Home />
