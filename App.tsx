a39c7c1b509aaf6585715a64403a0d01c7b1b2b1
matheus3006 <matheussouza18@live.com>
2022-07-09 21:01:23 -0300

    feat: add styled components global theme to project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
+import { Home } from './src/pages/Home';
 
-export default function App() {
+const App: React.FunctionComponent = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
