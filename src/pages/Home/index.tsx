e12b6bd6fe681b0f2f39e371d43006d13fe7e219
matheus3006 <matheussouza18@live.com>
2022-07-10 11:06:12 -0300

    feat: create base for user info display

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Container, Header } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  UserAvatarButton,
+  UserAvatar,
+  UserInfoDetail,
+  UserGreating,
+  UserName,
+} from './styles';
+
+import avatarDefault from '../../assets/avatar02.png';
 
 export const Home: React.FunctionComponent = () => {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={avatarDefault} />
+            </UserAvatarButton>
+
+            <UserInfoDetail>
+              <UserGreating>Olá, </UserGreating>
+              <UserName> Jorge </UserName>
+            </UserInfoDetail>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
