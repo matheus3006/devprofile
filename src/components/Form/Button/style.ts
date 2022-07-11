3e467af8cefb1884d7a94d3e20e99d9c347f5970
matheus3006 <matheussouza18@live.com>
2022-07-11 19:06:21 -0300

    feat: add Button to SignIn

--- src/components/Form/Button/style.ts
+++ src/components/Form/Button/style.ts
@@ -2,7 +2,17 @@ import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { TouchableOpacity } from 'react-native';
 
-export const Container = styled(TouchableOpacity)``;
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 5px;
+  padding: 18px;
+  margin-top: ${RFValue(16)}px;
+`;
 
-
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.dark};
+`;
