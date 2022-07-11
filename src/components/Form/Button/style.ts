6f7ff5b7eed9b5406632ee1dd80737d5eeecb465
matheus3006 <matheussouza18@live.com>
2022-07-11 18:18:05 -0300

    feat: add title extension to TouchableOpacityProps

---
+++ src/components/Form/Button/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { TouchableOpacity } from 'react-native';
+
+export const Container = styled(TouchableOpacity)``;
+
+
+export const Title = styled.Text``;
