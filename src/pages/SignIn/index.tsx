0a187016bcfa1b0ebd305b6a1df7f6343b119763
matheus3006 <matheussouza18@live.com>
2022-07-11 17:13:26 -0300

    feat: style and base signIn add

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Content, Title } from './styles';
+
+export const SignIn: React.FunctionComponent = () => {
+  return (
+    <Container>
+      <Content>
+        <Title>Faça seu login</Title>
+      </Content>
+    </Container>
+  );
+};
