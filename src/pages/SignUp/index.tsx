e02a4181ff8bd7af35cda682bbe9923036cd8813
matheus3006 <matheussouza18@live.com>
2022-07-14 20:54:18 -0300

    refactor: change SignUp page to work with react hook forms

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import { useForm, FieldValues } from 'react-hook-form';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from '../../components/Form/Button';
-import { Input } from '../../components/Form/Input/Index';
 import {
   BackToSignIn,
   BackToSignInTitle,
@@ -13,12 +13,29 @@ import {
   Title,
 } from './styles';
 import logo from '../../assets/logo.png';
+import { InputControl } from '../../components/Form/InputControl/Index';
 
 interface ScreenNavigationProp {
   goBack: () => void;
 }
 
+interface IFormInputs {
+  [name: string]: any;
+}
+
 export const SignUp: React.FunctionComponent = () => {
+  const { handleSubmit, control } = useForm<FieldValues>();
+
+  const handleSignUp = (form: IFormInputs) => {
+    const data = {
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    };
+
+    console.log(data);
+  };
+
   const { goBack } = useNavigation<ScreenNavigationProp>();
 
   return (
@@ -35,11 +52,29 @@ export const SignUp: React.FunctionComponent = () => {
           <Content>
             <Logo source={logo} />
             <Title>Cria sua Conta</Title>
-            <Input placeholder="Nome Completo" />
-            <Input placeholder="Email" />
-            <Input placeholder="Senha" />
+            <InputControl
+              placeholder="Nome Completo"
+              autoCorrect={false}
+              control={control}
+              name="name"
+            />
+            <InputControl
+              autoCapitalize="none"
+              autoCorrect={false}
+              control={control}
+              keyboardType="email-address"
+              placeholder="Email"
+              name="email"
+            />
+            <InputControl
+              name="password"
+              placeholder="Senha"
+              control={control}
+              autoCorrect={false}
+              secureTextEntry
+            />
 
-            <Button title="Criar conta" />
+            <Button title="Criar conta" onPress={handleSubmit(handleSignUp)} />
           </Content>
         </Container>
       </ScrollView>
