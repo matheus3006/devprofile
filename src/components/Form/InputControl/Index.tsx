39a9e57888c542eb404139739f4fe93bc61e83c8
matheus3006 <matheussouza18@live.com>
2022-07-15 07:38:29 -0300

    refactor: add error props and TextError for future validation on form

--- src/components/Form/InputControl/Index.tsx
+++ src/components/Form/InputControl/Index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { TextInputProps } from 'react-native';
 import { Control, Controller } from 'react-hook-form';
-import { Container } from './styles';
+import { Container, Error } from './styles';
 import { Input } from '../Input/Index';
 
 interface Props extends TextInputProps {
   control: Control;
   name: string;
+  error: string | undefined;
 }
 
 export const InputControl: React.FunctionComponent<Props> = ({
   control,
   name,
+  error,
   ...otherProps
 }) => {
   return (
@@ -22,7 +24,8 @@ export const InputControl: React.FunctionComponent<Props> = ({
           <Input onChangeText={onChange} value={value} {...otherProps} />
         )}
         name={name}
-      ></Controller>
+      />
+      {error && <Error> {error} </Error>}
     </Container>
   );
 };
