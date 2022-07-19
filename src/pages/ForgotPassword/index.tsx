1ff435ce447e5660335ba5115c0b61dd14bf2960
matheus3006 <matheussouza18@live.com>
2022-07-19 04:57:05 -0300

    feat: ForgotPassword init

---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import { api } from '../../services/api';
+
+import { useForm, FieldValues } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
+import { Button } from '../../components/Form/Button';
+import {
+  BackToSignIn,
+  BackToSignInTitle,
+  Container,
+  Content,
+  Icon,
+  Logo,
+  Title,
+} from './styles';
+import logo from '../../assets/logo.png';
+import { InputControl } from '../../components/Form/InputControl/Index';
+
+interface ScreenNavigationProp {
+  goBack: () => void;
+}
+
+interface IFormInputs {
+  [name: string]: any;
+}
+
+const formSchema = yup.object({
+  name: yup.string().required('Informe seu nome completo'),
+  email: yup.string().email('Email inválido').required('Informe o email'),
+  password: yup.string().required('Informe sua senha'),
+});
+
+export const ForgotPassword: React.FunctionComponent = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    resolver: yupResolver(formSchema),
+  });
+
+  const handleSignUp = async (form: IFormInputs) => {
+    const data = {
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    };
+
+    try {
+      await api.post('users', data);
+      Alert.alert(
+        'Cadastro realizado com sucesso',
+        'O cadastro foi realizado com sucesso',
+      );
+    } catch (err) {
+      Alert.alert(
+        'Erro no cadastro',
+        'Ocorreu um erro ao realizar o cadastro, tente novamente',
+      );
+    }
+  };
+
+  const { goBack } = useNavigation<ScreenNavigationProp>();
+
+  return (
+    <KeyboardAvoidingView
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Content>
+            <Logo source={logo} />
+            <Title>Cria sua Conta</Title>
+            <InputControl
+              placeholder="Nome Completo"
+              autoCorrect={false}
+              control={control}
+              name="name"
+              error={errors.name && errors.name.message}
+            />
+            <InputControl
+              autoCapitalize="none"
+              autoCorrect={false}
+              control={control}
+              keyboardType="email-address"
+              placeholder="Email"
+              name="email"
+              error={errors.email && errors.email.message}
+            />
+            <InputControl
+              name="password"
+              placeholder="Senha"
+              control={control}
+              autoCorrect={false}
+              secureTextEntry
+              error={errors.password && errors.password.message}
+            />
+
+            <Button title="Criar conta" onPress={handleSubmit(handleSignUp)} />
+          </Content>
+        </Container>
+      </ScrollView>
+      <BackToSignIn onPress={() => goBack()}>
+        <Icon name="arrow-left" />
+        <BackToSignInTitle> Entrar na minha Conta </BackToSignInTitle>
+      </BackToSignIn>
+    </KeyboardAvoidingView>
+  );
+};
