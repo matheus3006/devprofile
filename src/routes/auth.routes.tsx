ad2c3ed5c7434c413a8a170bb5f0266ba5842bc1
matheus3006 <matheussouza18@live.com>
2022-07-24 12:43:34 -0300

    feat: add routers

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,7 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
 import { ForgotPassword } from '../pages/ForgotPassword';
-import { ResetPassword } from '../pages/ResetPassoword';
+import { ResetPassword } from '../pages/ResetPassword';
+
 const Auth = createNativeStackNavigator();
 
 export const AuthRoutes: React.FunctionComponent = () => {
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AuthRoutes } from './auth.routes';
 import { AppRoutes } from './app.routes';
 import { useAuth } from '../context/AuthContext';
+
 export const Routes: React.FunctionComponent = () => {
   const { user } = useAuth();
 
